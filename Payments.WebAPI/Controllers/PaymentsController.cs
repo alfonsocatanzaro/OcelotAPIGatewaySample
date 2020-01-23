375eff42478864a39cf2ed181e87dae3d06c78e4
Alfonso Catanzaro [eADE] <a.catanzaro@eade.it>
2020-01-23 17:42:51 +0100

    initial commit

---
+++ Payments.WebAPI/Controllers/PaymentsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Payments.WebAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class PaymentsController : ControllerBase
+	{
+		// GET api/values
+		[HttpGet]
+		public ActionResult<IEnumerable<string>> Get()
+		{
+			return new string[] { "Payment 1", "Payment 2", "Payment n" };
+		}
+
+		// GET api/values/5
+		[HttpGet("{id}")]
+		public ActionResult<string> Get(int id)
+		{
+			return $"Payment {id}";
+		}
+
+	}
+}
---
+++ Products.WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Products.WebAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ProductsController : ControllerBase
+	{
+		// GET api/values
+		[HttpGet]
+		public ActionResult<IEnumerable<string>> Get()
+		{
+			return new string[] { "Product 1", "Product 2", "Product n" };
+		}
+
+		// GET api/values/5
+		[HttpGet("{id}")]
+		public ActionResult<string> Get(int id)
+		{
+			
+			return $"Product {id}";
+		}
+
+		
+	}
+}
